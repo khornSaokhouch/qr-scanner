79c6114d9078ef1384788ac61d8be738725a4bb7
macm4 <vuthulsmile006@gmail.com>
2026-09-19 15:22:55 +0700

    add analysis

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
         <ThemeProvider>
           {/* Sticky Header */}
           <Navbar />
+          <Analytics />
 
           {/* Page Content */}
           <main className="flex-1">{children}</main>
